ce664acd47770019b91a78c1fe15f2adc97d3e84
Boa Matule <boamatule@Sivteki-MacBook-Air.local>
2019-11-29 16:08:49 +0100

    Creates a simple card using react-native-elements.

--- screens/AlbumsScreen.js
+++ screens/AlbumsScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
+import { Text, Card, Button, Icon } from  'react-native-elements';
 
 export default class AlbumsScreen extends React.Component {
   static navigationOptions = {
@@ -9,7 +10,17 @@ export default class AlbumsScreen extends React.Component {
   render() {
     return (
       <ScrollView style={styles.container}>
-       <Text>I am albums screen</Text>
+          <Card
+            title='HELLO WORLD'>
+            <Text style={{marginBottom: 10}}>
+              The idea with React Native Elements is more about component structure than actual design.
+            </Text>
+            <Button
+              icon={<Icon name='code' color='#ffffff' />}
+              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+            title='VIEW NOW' />
+        </Card>
+
       </ScrollView>
     )
 
