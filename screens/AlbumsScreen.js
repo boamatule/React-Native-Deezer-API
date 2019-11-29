21b665cc1c1c6577f198c6734084ce15a540cfe8
Boa Matule <boamatule@Sivteki-MacBook-Air.local>
2019-11-29 17:08:26 +0100

    Setups different states for the Albums screen.

--- screens/AlbumsScreen.js
+++ screens/AlbumsScreen.js
@@ -7,20 +7,44 @@ export default class AlbumsScreen extends React.Component {
     title: 'Albums',
   };
 
+  constructor() {
+    super();
+    this.state = {
+      cardName: 'Some Album'
+    }
+    this.cardNumber = 0;
+  }
+
+  changeCardName() {
+    this.setState({
+      cardName: `I am new CardName ${this.cardNumber}`
+    });
+    this.cardNumber++;
+  }
+
   render() {
+    const { cardName } = this.state;
     return (
       <ScrollView style={styles.container}>
-          <Card
-            title='HELLO WORLD'>
-            <Text style={{marginBottom: 10}}>
-              The idea with React Native Elements is more about component structure than actual design.
-            </Text>
-            <Button
-              icon={<Icon name='code' color='#ffffff' />}
-              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+        <Card
+          title={cardName}>
+          <Text style={{marginBottom: 10}}>
+            The idea with React Native Elements is more about component structure than actual design.
+          </Text>
+          <Button
+            icon={<Icon name='code' color='#ffffff' />}
+            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
             title='VIEW NOW' />
         </Card>
 
+        <Button
+          icon={<Icon name='code' color='#ffffff' />}
+          buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+          title='Change Card Name' 
+          onPress={() => { this.changeCardName() }} />
+          
+
+        
       </ScrollView>
     )
 
