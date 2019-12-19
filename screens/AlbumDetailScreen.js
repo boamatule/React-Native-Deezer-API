b75b12fa47563e1942933e5e77e0ba799ffdd29d
Boa Matule <boamatule@Sivteki-MacBook-Air.local>
2019-12-19 18:02:36 +0100

    Display tracks...

--- screens/AlbumDetailScreen.js
+++ screens/AlbumDetailScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View, Text } from  'react-native';
+import { ScrollView, StyleSheet, View } from  'react-native';
 import * as actions from '../actions';
-import { Avatar, Icon } from 'react-native-elements';
+import { Avatar,Text, Icon, Divider, List, ListItem } from 'react-native-elements';
 
 export default class AlbumDetailScreen extends React.Component {
   static navigationOptions = {
@@ -26,30 +26,59 @@ export default class AlbumDetailScreen extends React.Component {
       .catch(error => console.error(error))
     }
 
+    renderTracks() {
+      const {tracks} = this.state;
+  
+      if (tracks && tracks.length > 0) {
+        return tracks.map((track, index) => {
+  
+          return (
+            <ListItem key={index}
+                      title={track.title}
+                      leftIcon={{name: 'play-arrow'}}
+                      onPress={() => {}}
+                      rightIcon={
+                        <Icon raised
+                              name='star'
+                              type='font-awesome'
+                              color='#f50'
+                              onPress={() => {}}
+                        />
+                      }
+            />
+          )
+        })
+      }
+    }
+  
     render() {
       const album = this.props.navigation.getParam('album', {});
       const artist = this.props.navigation.getParam('artist', '');
-
+  
       if (album.id) {
         return (
           <ScrollView style={styles.container}>
             <View>
-              <Avatar xlarge rounded source={{uri: album.cover_medium}}></Avatar>
+              <Avatar xlarge rounded source={{uri: album.cover_medium}}> </Avatar>
               <View>
-                <Text h4>{album.title} </Text>
-                <Text h4>{artist} </Text>
-              </View>
+                <Text h4> {album.title} </Text>
+                <Text h4> {artist} </Text>
                 <Icon raised
-                  name='play'
-                  type='font-awesome'
-                  color='#f50'
-                  size={30}
-                  onPress={() => {}} />
+                      name='play'
+                      type='font-awesome'
+                      color='#f50'
+                      size={30}
+                      onPress={() => {}} />
+              </View>
             </View>
+            <Divider style={{backgroundColor: 'black'}}/>
+            <List>
+            { this.renderTracks() }
+            </List>
           </ScrollView>
-        )  
+        )
       } else {
-        <View><Text> Loading...</Text></View>
+        <View> <Text> Loading... </Text> </View>
       }
     }
   }
