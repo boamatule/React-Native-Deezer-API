821077638a92b7e12c0845e95616f79a6406a5d0
Boa Matule <boamatule@Sivteki-MacBook-Air.local>
2019-12-22 02:32:40 +0100

    Use the FlatList to give more performance to our App

--- components/CardList.js
+++ components/CardList.js
@@ -1,32 +1,50 @@
 import React from 'react';
-import { View }  from 'react-native';
+import { View, FlatList }  from 'react-native';
 import { Card, Button, Text, Icon } from 'react-native-elements';
 
 export class CardList extends React.Component {
   renderData() {
       const { data, imageKey, titleKey, bottonText, bottonView } = this.props;
 
-      return data.map((item, index) => {
-        return (
-          <Card
-            key={index}
-            title={item[titleKey]}
-            image={{uri: item[imageKey]}}>
-            { bottonView(item) }
-          </Card>
-        )
-      })
+      // return data.map((item, index) => {
+      //   return (
+      //     <Card
+      //       key={index}
+      //       title={item[titleKey]}
+      //       image={{uri: item[imageKey]}}>
+      //       { bottonView(item) }
+      //     </Card>
+      //   )
+      // })
+
+      return (
+        <FlatList data={data}
+                  keyExtractor={(item, index) => index.toString()}
+                  renderItem={({item, index}) => {
+                    return (
+                      <Card
+                        key={index}
+                        title={item[titleKey]}
+                        image={{uri: item[imageKey]}}>
+                        { bottonView(item) }
+                      </Card>
+                    )
+                  }}>
+        </FlatList>
+      )
     }
 
-  render() {
-    const { data } = this.props;
+    render() {
+      const { data } = this.props;
 
-    if (data && data.length > 0) {
-      return this.renderData();
-    } else {
-      return (
-        <View></View>
-      )
-    }  
-  }
+      if (data && data.length > 0) {
+        return this.renderData();
+      } else {
+        return (
+          <View> </View>
+        )
+      }  
+    }
 }
+
+
